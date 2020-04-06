02424564b2dabe23544ef2a410c8875f2b4c1d89
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-04-06 20:50:28 +0300

    Removed isPoisoned field & new constructor

--- src/Student.java
+++ src/Student.java
@@ -1,40 +1,12 @@
-import java.util.Random;
-
 public class Student {
     private int id;
     private int amountOfLabs;
     private String subject;
-    private boolean isPoisoned;
 
-    public Student(int id) {
+    public Student(int id, int amountOfLabs, String subject) {
         this.id = id;
-        isPoisoned = false;
-        Random random = new Random();
-        int labs = random.nextInt(3);
-        switch (labs) {
-            case 0:
-                amountOfLabs = 10;
-                break;
-            case 1:
-                amountOfLabs = 20;
-                break;
-            case 2:
-                amountOfLabs = 100;
-                break;
-        }
-
-        int subjectNumber = random.nextInt(3);
-        switch (subjectNumber) {
-            case 0:
-                subject = "Физика";
-                break;
-            case 1:
-                subject = "Вышмат";
-                break;
-            case 2:
-                subject = "ООП";
-                break;
-        }
+        this.amountOfLabs = amountOfLabs;
+        this.subject = subject;
     }
 
     public int getAmountOfLabs() {
@@ -49,11 +21,4 @@ public class Student {
         return id;
     }
 
-    public boolean isPoisoned() {
-        return isPoisoned;
-    }
-
-    public void setPoisoned(boolean poisoned) {
-        isPoisoned = poisoned;
-    }
 }
