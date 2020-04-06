fea8cc4f1762ee384ecffb97f9b73eeb21d10a9b
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2020-04-06 20:50:51 +0300

    Move random here

--- src/Producer.java
+++ src/Producer.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 
 public class Producer implements Runnable {
@@ -14,13 +15,43 @@ public class Producer implements Runnable {
 
     private void process() throws InterruptedException {
         for (int i = 0; i < 103; i++) {
-            Student student = new Student(i);
-            if (i >= 100) {
-                student.setPoisoned(true);
-            } else {
-                System.out.println("Студент " + student.getId() + " встал в очередь");
+            Random random = new Random();
+            int labs = random.nextInt(3);
+            int amountOfLabs = 0;
+            switch (labs) {
+                case 0:
+                    amountOfLabs = 10;
+                    break;
+                case 1:
+                    amountOfLabs = 20;
+                    break;
+                case 2:
+                    amountOfLabs = 100;
+                    break;
             }
-            queue.put(student);
+
+            int subjectNumber = random.nextInt(3);
+            String subject = "";
+            switch (subjectNumber) {
+                case 0:
+                    subject = "Физика";
+                    break;
+                case 1:
+                    subject = "Вышмат";
+                    break;
+                case 2:
+                    subject = "ООП";
+                    break;
+            }
+
+            if (i >= 100) {
+                Student student = new Student(i, -1, subject);
+                queue.put(student);
+            } else {
+                Student student = new Student(i, amountOfLabs, subject);
+                System.out.println("Студент " + student.getId() + " встал в очередь");
+                queue.put(student);
+            }
             Thread.sleep(1000);
         }
     }
